26b293dbff41e607b27a36a3fa1ea257df391ef7
YasmineRehman <chacharmeenu@gmail.com>
2023-12-19 15:59:26 +0500

    my commit

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -13,25 +13,25 @@ export default class Services extends Component {
         icon: <FaCocktail />,
         title: "free cocktails",
         info:
-          "room3.",
+          "For those seeking excitement, our resort offers a myriad of water sports, excursions, and recreational activities. Whether it's diving into the azure waters, exploring the local culture, or simply lounging by the pool, there's something for every adventurer.",
       },
       {
         icon: <FaHiking />,
         title: "endless hiking",
         info:
-          "room3",
+          "For those seeking excitement, our resort offers a myriad of water sports, excursions, and recreational activities. Whether it's diving into the azure waters, exploring the local culture, or simply lounging by the pool, there's something for every adventurer",
       },
       {
         icon: <FaShuttleVan />,
         title: "free shuttle",
         info:
-          "room2",
+          "Rejuvenate your mind, body, and soul at our world-class spa facilities. Our skilled therapists offer a range of treatments inspired by traditional healing practices, providing a holistic approach to wellness against the backdrop of the soothing ocean waves",
       },
       {
         icon: <FaBeer />,
         title: "storages beer",
         info:
-          "room1",
+          "Wake up to breathtaking views of the ocean or lush gardens, creating a haven of relaxation.",
       },
     ],
   };
